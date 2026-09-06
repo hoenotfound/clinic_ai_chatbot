00c7fcbaeee4a65456c1948b4008dbcc4a58e7a3
hoenotfound <91048208+hoenotfound@users.noreply.github.com>
2026-09-06 21:57:22 +0800

    Fix multichannel follow-up test isolation

--- test/followUpMultiChannel.test.js
+++ test/followUpMultiChannel.test.js
@@ -12,6 +12,7 @@ const { runAutomatedFollowUps } = require("../src/services/followUpService");
 
 const originals = {
   findCandidates: followUpRepo.findCandidates,
+  getNextCandidateDueAt: followUpRepo.getNextCandidateDueAt,
   saveIfStillEligible: followUpRepo.saveIfStillEligible,
   saveSocialImageCompanion: followUpRepo.saveSocialImageCompanion,
   markStaleClaimsUnconfirmed: followUpRepo.markStaleClaimsUnconfirmed,
@@ -27,6 +28,7 @@ const originals = {
 test.after(() => {
   Object.assign(followUpRepo, {
     findCandidates: originals.findCandidates,
+    getNextCandidateDueAt: originals.getNextCandidateDueAt,
     saveIfStillEligible: originals.saveIfStillEligible,
     saveSocialImageCompanion: originals.saveSocialImageCompanion,
     markStaleClaimsUnconfirmed: originals.markStaleClaimsUnconfirmed,
@@ -61,6 +63,7 @@ function enableTool({ imageUrl = "" } = {}) {
 test.beforeEach(() => {
   enableTool();
   followUpRepo.markStaleClaimsUnconfirmed = async () => [];
+  followUpRepo.getNextCandidateDueAt = async () => null;
   followUpRepo.saveSocialImageCompanion = async () => null;
   contactsRepo.setDeliveryAttention = async () => {};
   pipelineRepo.markContactedForContact = async () => false;
